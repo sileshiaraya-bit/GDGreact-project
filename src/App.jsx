cc7c7c15a5006749dcd4aa813c2889a572213d10
Sileshi Biadgilgn <sileshiaraya@gmail.com>
2025-12-10 12:15:07 +0300

    add exercise

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
+import { useState } from "react";
+
+
 function App() {
+  const [count, setState,]=useState(1)
+  function increment(){
+    setCout(count + 2);
+    console.log("Local;", cout);
+  }
+  function decrement() {
+    setCount(count - 2);
+    console.log("Local",count);
+  }
   return (
-    <div className="App">
-      <h1>Hello World!</h1>
+    <div>
+      <p>count</p>
+      <p>Local Local variable: {count}</p>
+      <button onClick={increment}> increase Local </button>
+      <button on click={decrement}> decrease Local</button>
+      
     </div>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import { createRoot } from 'react-dom/client'
+import App from './App'
 
 createRoot(document.getElementById('root')).render(
-  <h1>Hello React!</h1>
+  <App/>
 )  
