18ece6563bbe9299e318e5df2fec1e5d36441c3e
Sileshi Biadgilgn <sileshiaraya@gmail.com>
2025-12-10 12:15:18 +0300

    add exercise

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,11 @@
-import { useState } from "react";
 
+import MiniCalculator from "./MiniCalculator";
 
 function App() {
-  const [count, setState,]=useState(1)
-  function increment(){
-    setCout(count + 2);
-    console.log("Local;", cout);
-  }
-  function decrement() {
-    setCount(count - 2);
-    console.log("Local",count);
-  }
   return (
-    <div>
-      <p>count</p>
-      <p>Local Local variable: {count}</p>
-      <button onClick={increment}> increase Local </button>
-      <button on click={decrement}> decrease Local</button>
-      
+    <div className="container">
+
+      <MiniCalculator/>
     </div>
   );
 }
---
+++ src/Header.jsx
@@ -0,0 +1,10 @@
+function Header(){
+  return(
+    <div>
+      <h1>my first app</h1>
+    </div>
+     
+    
+  );
+}
+export default Header
